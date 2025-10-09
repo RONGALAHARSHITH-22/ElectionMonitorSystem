ae1ef4fbc5f06fff819ad4aac6d5dc3853090303
Sana Phanendra <232562183+PhanendraSana-32649@users.noreply.github.com>
2025-10-09 10:52:59 +0530

    Create header.jsx

---
+++ header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Election Monitoring System</h1>
+      <p>Ensuring Transparency, Trust, and Fair Elections</p>
+    </header>
+  )
+}
